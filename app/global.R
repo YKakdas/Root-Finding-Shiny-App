b1a1853524fd0dcc3813add9117e522e391f8f3d
Tom Mann <mann.tom@kabelmail.de>
2022-11-18 20:34:12 -0500

    added new server and page files

--- app/global.R
+++ app/global.R
@@ -11,11 +11,20 @@ server <- function(input, output, session) {
   source('servers/fixedpointserver.R', local = TRUE)
   source('servers/bisectionserver.R', local = TRUE)
   source('servers/dashboardserver.R', local = TRUE)
+  source('servers/newtonserver.R', local = TRUE)
+  source('servers/secantserver.R', local = TRUE)
+  source('servers/regulaserver.R', local = TRUE)
+  source('servers/halleyserver.R', local = TRUE)
+  
 }
 
 source("pages/homepage.R")
 source("pages/fixedpointpage.R")
 source("pages/bisectionpage.R")
+source("pages/newtonpage.R")
+source("pages/secantpage.R")
+source("pages/regulapage.R")
+source("pages/halleypage.R")
 
 source('dashboard.R')
 
@@ -23,3 +32,5 @@ source("algorithms/bisection.R")
 source("algorithms/fixedpoint.R")
 source("algorithms/newton.R")
 source("algorithms/secant.R")
+source("algorithms/regula.R")
+source("algorithms/halley.R")
